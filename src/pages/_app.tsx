42497b82d8a2de97fe5a7e9d883732c396205a27
amalcaraz <amalcaraz89@gmail.com>
2023-02-26 21:36:58 +0100

    Added basic config for using aleph-core components library + fixed problems with google fonts & font awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+import '../styles/font.css'
+
+// @IMPORTANT: Read https://fontawesome.com/docs/web/use-with/react/use-with#next-js
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { themes, GlobalStyle } from '@aleph-front/aleph-core'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={themes.dark}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
+  )
 }
