a7102afcc9f4a3cb808ada64f0bd3e3f2d0548a9
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-02-27 09:41:49 +0100

    add: favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Aleph.im Cloud Solution</title>
+        <title>Aleph.im Cloud Solutions</title>
         <meta name="description" content="Aleph.im Cloud Solution" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <main>
         <Container>
