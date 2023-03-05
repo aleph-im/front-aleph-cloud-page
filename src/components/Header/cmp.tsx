c9cd99a5d4fb9df9d6cc448c7c76a9dfab42fa76
amalcaraz <amalcaraz89@gmail.com>
2023-03-05 15:17:35 +0100

    fixed connect icon color

--- src/components/Header/cmp.tsx
+++ src/components/Header/cmp.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Button, Icon } from '@aleph-front/aleph-core'
 import { StyledHeader, StyledButton, StyledNavbar } from './styles'
 import { HeaderProps } from './types'
+import { useTheme } from 'styled-components'
 
 export const Header = (props: HeaderProps) => {
+  const theme = useTheme()
+
   return (
     <StyledHeader>
       <StyledNavbar
@@ -13,7 +16,7 @@ export const Header = (props: HeaderProps) => {
         ]}
         navButtons={[
           <StyledButton key="link" forwardedAs="a" disabled><Icon name="link-simple-slash" /></StyledButton>,
-          <StyledButton key="connect" forwardedAs="a">Connect <Icon name="meteor" size='lg' className='ml-xs' /></StyledButton>,
+          <StyledButton key="connect" forwardedAs="a">Connect <Icon name="meteor" size='lg' className='ml-xs' color={theme.color.main0} /></StyledButton>,
         ]}
       />
     </StyledHeader>
