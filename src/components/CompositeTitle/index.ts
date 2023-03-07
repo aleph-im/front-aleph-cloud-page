acc9b416cda466563fdf60871c2d5be0ad97c778
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-03-07 18:29:29 +0100

    feat: composite title

---
+++ src/components/CompositeTitle/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./cmp";
+export type { CompositeTitleProps } from "./types";
---
+++ src/components/CompositeTitle/types.tsx
@@ -0,0 +1,10 @@
+export type CompositeTitleProps = {
+  title: string
+  number: number | string
+  color: string
+  label?: string
+}
+
+export type StyledCompositeTitleLabelProps = {
+  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
