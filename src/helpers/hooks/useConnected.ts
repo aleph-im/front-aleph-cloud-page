c15c8ec4cc01289657fa595d2f69fd2ec3d3676f
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-03-07 18:30:26 +0100

    feat: useConnected hook

---
+++ src/helpers/hooks/useConnected.ts
@@ -0,0 +1,19 @@
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { AppStateContext } from "@/pages/_app";
+
+/**
+ * Redirects to a route if the user is not connected
+ *
+ * @param route The path to redirect to (defaults to "/")
+ */
+export default function useConnected(route: string = "/") {
+  const { state, dispatch } = useContext(AppStateContext);
+
+  const router = useRouter();
+  useEffect(() => {
+    if (!state.account) {
+      router.replace(route);
+    }
+  });
+}
