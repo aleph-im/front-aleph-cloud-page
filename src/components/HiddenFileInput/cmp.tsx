1b2288d1fcee7e17a2f6a772c6e8de2c45a81e8a
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-03-28 18:53:03 +0200

    enh: HiddenFileInput display

--- src/components/HiddenFileInput/cmp.tsx
+++ src/components/HiddenFileInput/cmp.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Button } from '@aleph-front/aleph-core';
+import { Button, Icon } from '@aleph-front/aleph-core';
 import { HiddenFileInputProps } from './types';
 import { StyledHiddenFileInput } from './styles';
+import { ellipseAddress } from '@/helpers/utils';
 
 const HiddenFileInput = ({ onChange, accept, children }: HiddenFileInputProps) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const [inMemoryFile, setInMemoryFile] = React.useState<File | null>(null);
 
   const handleClick = () => {
     if(inputRef.current !== null)
@@ -16,17 +18,26 @@ const HiddenFileInput = ({ onChange, accept, children }: HiddenFileInputProps) =
     const target = e.target as HTMLInputElement;
     const { files } = target;
 
+    
     if(files){
       const fileUploaded = files[0];
+      setInMemoryFile(fileUploaded);
       onChange(fileUploaded);
     }
   };
 
   return (
     <>
-      <Button onClick={handleClick} type="button" color="main0" kind="neon" size="regular" variant="primary">
-        {children}
-      </Button>
+      {
+        inMemoryFile ?
+        <Button onClick={() => setInMemoryFile(null)} type="button" color="main2" kind="neon" size="regular" variant="tertiary">
+          {ellipseAddress(inMemoryFile.name)} <Icon name="trash" className="ml-md" />
+        </Button>
+        :
+        <Button onClick={handleClick} type="button" color="main0" kind="neon" size="regular" variant="primary">
+          {children}
+        </Button>
+      }
 
       <StyledHiddenFileInput
         type="file"
