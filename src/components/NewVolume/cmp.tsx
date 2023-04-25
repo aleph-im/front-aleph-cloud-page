a93eb439932685ebb58551aeac7e3b43a18425e8
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-04-25 10:03:14 +0200

    add: NewVolume cmp

---
+++ src/components/NewVolume/cmp.tsx
@@ -0,0 +1,161 @@
+import { humanReadableSize, isValidItemHash } from "@/helpers/utils";
+import { Tabs, Icon, TextInput, Checkbox, Button } from "@aleph-front/aleph-core";
+import HiddenFileInput from "../HiddenFileInput";
+import NoisyContainer from "../NoisyContainer";
+import { RemoveVolumeProps, NewVolumeProps } from "./types";
+import { useMemo } from "react";
+
+const RemoveVolume = ({ removeCallback }: RemoveVolumeProps) => (
+  <div className="my-md text-right">
+    <Button type="button" onClick={removeCallback} color="main2" variant="secondary" kind="neon" size="regular">
+      Remove
+    </Button>
+  </div>
+)
+
+const NewVolume = ({ volumeMountpoint, volumeRefHash, volumeName, volumeSize, volumeSrc, volumeUseLatest, handleMountpointChange, handleNameChange, handleSizeChange, handleSrcChange, handleUseLatestChange, handleRefHashChange, handleVolumeType, removeCallback }: NewVolumeProps) => {
+  const namePrefix = useMemo(() => crypto.randomUUID(), [])
+  
+  return (
+<Tabs align="left"
+      onTabChange={handleVolumeType}
+      tabs={[
+      {
+        name: 'New volume',
+        component: (
+          <div className="my-lg">
+            <p>Create and configure new volumes for your web3 function by either uploading a dependency file or a squashfs volume. Volumes play a crucial role in managing dependencies and providing a volume within your application.</p>
+
+            <NoisyContainer>
+              <div className="my-md d-flex flex-jc-sb">
+                <HiddenFileInput value={volumeSrc} onChange={handleSrcChange}>
+                  Upload squashfs volume <Icon name="arrow-up" className="ml-sm" />
+                </HiddenFileInput>
+
+                <strong>or</strong>
+
+                <div className="unavailable-content">
+                  <HiddenFileInput onChange={handleSrcChange}>
+                    Upload dependency file <Icon name="arrow-up" className="ml-sm" />
+                  </HiddenFileInput>
+                </div>
+              </div>
+
+              <div className="my-md">
+                <TextInput
+                  label="Mount"
+                  placeholder="/mount/opt"
+                  onChange={handleMountpointChange}
+                  value={volumeMountpoint}
+                  name={namePrefix + '_mount'} />
+              </div>
+
+              <div className="my-md">
+                <TextInput
+                  label="Size"
+                  disabled
+                  value={humanReadableSize((volumeSrc?.size || 0) / 1000)}
+                  name={namePrefix + '_size'} />
+              </div>
+
+              <div className="my-md">
+                <Checkbox
+                  label="Use latest version"
+                  checked={volumeUseLatest}
+                  onChange={handleUseLatestChange} />
+              </div>
+
+              {
+                removeCallback !== undefined && <RemoveVolume removeCallback={removeCallback} />
+              }
+            </NoisyContainer>
+          </div>
+        )
+      },
+      {
+        name: 'Existing volume',
+        component: (
+          <div className="my-lg">
+            <p>Link existing volumes to your web3 function by pasting the reference hash associated with each volume. Volumes are an essential component for managing persistent data storage and dependencies within your application.</p>
+
+            <NoisyContainer>
+              <div className="my-md">
+                <TextInput
+                  label="Mount"
+                  placeholder="/mount/opt"
+                  onChange={handleMountpointChange}
+                  value={volumeMountpoint}
+                  name={namePrefix + '_mount'} />
+              </div>
+
+              <div className="my-md">
+                <TextInput
+                  label="Item hash"
+                  placeholder="3335ad270a571b..."
+                  onChange={handleRefHashChange}
+                  value={volumeRefHash}
+                  error={
+                    (volumeRefHash && !isValidItemHash(volumeRefHash || '')) ? { message: "Invalid hash" } : undefined
+                  }
+                  name={namePrefix + '_refhash'} />
+              </div>
+
+              <div className="my-md">
+                <Checkbox
+                  label="Use latest version"
+                  onChange={handleUseLatestChange} />
+              </div>
+
+              {
+                removeCallback !== undefined && <RemoveVolume removeCallback={removeCallback} />
+              }
+            </NoisyContainer>
+          </div>
+        )
+      },
+      {
+        name: 'Persistent storage',
+        component: (
+          <div className="my-lg">
+            <p>Create and configure persistent storage for your web3 functions, enabling your application to maintain data across multiple invocations or sessions. You can set up a customized storage solution tailored to your application&apos;s requirements.</p>
+
+            <NoisyContainer>
+              <div className="my-md">
+                <TextInput
+                  label="Volume name"
+                  placeholder="Redis volume"
+                  onChange={handleNameChange}
+                  value={volumeName}
+                  name={namePrefix + '_name'}
+                  error={volumeName === "" ? new Error('Please provide a name') : undefined} />
+              </div>
+
+              <div className="my-md">
+                <TextInput
+                  label="Mount"
+                  placeholder="/mount/opt"
+                  onChange={handleMountpointChange}
+                  value={volumeMountpoint}
+                  name={namePrefix + '_mount'} />
+              </div>
+
+              <div className="my-md">
+                <TextInput
+                  label="Size (GB)"
+                  placeholder="2"
+                  onChange={handleSizeChange}
+                  value={volumeSize}
+                  name={namePrefix + '_size'} />
+              </div>
+
+              {
+                removeCallback !== undefined && <RemoveVolume removeCallback={removeCallback} />
+              }
+            </NoisyContainer>
+          </div>
+        )
+      },
+    ]} />
+)}
+
+export default NewVolume
---
+++ src/components/NewVolume/types.tsx
@@ -0,0 +1,20 @@
+export type RemoveVolumeProps = {
+  removeCallback: () => void
+}
+
+export type NewVolumeProps = {
+  volumeName?: string
+  handleNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  volumeMountpoint?: string
+  handleMountpointChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  volumeSize?: number
+  handleSizeChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  volumeUseLatest?: boolean
+  handleUseLatestChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  volumeSrc?: File
+  handleSrcChange: (f: File | undefined) => void
+  volumeRefHash?: string
+  handleRefHashChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  volumeType?: number
+  handleVolumeType: (t: number) => void
+} & Partial<RemoveVolumeProps>
