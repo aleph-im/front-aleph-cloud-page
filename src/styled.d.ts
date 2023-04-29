ecf4d1a7f6c09bd31a941d5820ab693fb5e47855
amalcaraz <amalcaraz89@gmail.com>
2023-04-29 02:19:23 +0200

    fixed missing types

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components'
 import { CoreTheme } from '@aleph-front/aleph-core'
 
 declare module 'styled-components' {
-  export type DefaultTheme = CoreTheme
+  export interface DefaultTheme extends CoreTheme {}
 }
