e03567627e1178115ad9d8532c9dceeafbcb5081
amalcaraz <amalcaraz89@gmail.com>
2023-05-02 15:17:03 +0200

    fixed footer align

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,8 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStylesOverride = createGlobalStyle`  
+  body, #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
   main {
-    min-height: 800px;
+    flex: 1 0 auto;
   }
 
   /* FIXME: */
@@ -23,8 +29,7 @@ export const GlobalStylesOverride = createGlobalStyle`
   }
 
   /* FIXME: */
-  table: {
+  table {
     max-width: 100%;
   }
-
 `
