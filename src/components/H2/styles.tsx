f030fe22131116664f01cd811c58881737f15dc5
amalcaraz <amalcaraz89@gmail.com>
2023-05-02 15:05:24 +0200

    fix wrong displayed h2 labels on firefox

--- src/components/H2/styles.tsx
+++ src/components/H2/styles.tsx
@@ -1,5 +1,5 @@
 import { addClasses, TextGradient } from '@aleph-front/aleph-core'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const StyledH2 = styled(TextGradient).attrs((props) => {
   return {
@@ -16,12 +16,13 @@ export const StyledH2 = styled(TextGradient).attrs((props) => {
 export const StyledLabel = styled.span.attrs(
   addClasses('gr-main0 tp-header fs-md'),
 )`
-  position: absolute;
-  top: -0.25em;
-  left: 100%;
-  margin-left: 10px;
-  padding: 0.375rem 0.75rem;
-  mask: linear-gradient(#fff 0 0) text, linear-gradient(#fff 0 0);
-  mask-composite: exclude;
-  -webkit-mask-composite: xor;
+  ${({ theme }) => css`
+    position: absolute;
+    top: -0.25em;
+    left: 100%;
+    margin-left: 10px;
+    padding: 0.375rem 0.75rem;
+    -webkit-text-fill-color: ${theme.color.background};
+    color: ${theme.color.background};
+  `}
 `
