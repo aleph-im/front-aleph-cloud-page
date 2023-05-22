f421d1c5e0371b0d6e67a9bf6dacf2737b1e7034
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-05-22 11:17:47 +0200

    feat (wip): add anchor to path when clicking link

--- src/hooks/useScrollTo.ts
+++ src/hooks/useScrollTo.ts
@@ -1,7 +1,17 @@
+import { useRouter } from 'next/router'
 import { RefObject, useCallback, useRef } from 'react'
 
-function getHandleScroll(ref: RefObject<HTMLElement>) {
+function getHandleScroll(
+  ref: RefObject<HTMLElement>,
+  router: ReturnType<typeof useRouter>,
+) {
   if (!ref.current) return
+  if (ref.current.id) {
+    router.push({ hash: ref.current.id }, undefined, {
+      shallow: true,
+      scroll: false,
+    })
+  }
 
   // @note: Fix for sticky header (works better than scroll-margin-top + scrollIntoView)
   const { top } = ref.current.getBoundingClientRect()
@@ -13,8 +23,12 @@ export type UseScrollToReturn = [RefObject<HTMLElement>, () => void]
 export function useScrollTo(_ref?: RefObject<HTMLElement>): UseScrollToReturn {
   const innerRef = useRef(null)
   const ref = _ref || innerRef
+  const router = useRouter()
 
-  const handleScroll = useCallback(() => getHandleScroll(ref), [ref.current])
+  const handleScroll = useCallback(
+    () => getHandleScroll(ref, router),
+    [ref.current],
+  )
 
   return [ref, handleScroll]
 }
