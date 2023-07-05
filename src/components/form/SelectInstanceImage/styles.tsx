61e4a639d7bc739f8b3fbf9d5e905b1c9f8dfae5
amalcaraz <amalcaraz89@gmail.com>
2023-07-05 11:33:46 +0200

    fix: responsive fixes

--- src/components/form/SelectInstanceImage/styles.tsx
+++ src/components/form/SelectInstanceImage/styles.tsx
@@ -1,6 +1,9 @@
 import styled, { css } from 'styled-components'
 import tw from 'twin.macro'
-import { getGlowHoverEffectCss } from '@aleph-front/aleph-core'
+import {
+  getGlowHoverEffectCss,
+  getResponsiveCss,
+} from '@aleph-front/aleph-core'
 
 export const StyledFlatCardContainer = styled.div`
   ${tw`flex items-center justify-start md:justify-center flex-nowrap md:flex-wrap gap-6`}
@@ -12,8 +15,8 @@ export const StyledFlatCard = styled.div<{
 }>`
   ${({ $selected, $disabled }) => css`
     ${tw`flex flex-col items-center justify-center shrink-0 cursor-pointer transition-all duration-300`}
-    width: 13.875rem;
     height: 10.125rem;
+    width: 13.875rem;
     background-color: #ffffff1a;
     border-radius: 1.5rem;
     ${$selected && `border: 1px solid white;`}
@@ -22,5 +25,12 @@ export const StyledFlatCard = styled.div<{
     &:hover {
       ${!$disabled && getGlowHoverEffectCss('main0')}
     }
+
+    ${getResponsiveCss(
+      'md',
+      css`
+        width: 30%;
+      `,
+    )}
   `}
 `
