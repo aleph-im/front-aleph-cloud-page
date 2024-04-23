0cf0a952cc1979459c66aa5d512d32fa217e530b
amalcaraz <amalcaraz89@gmail.com>
2024-04-23 15:24:57 +0200

    feat: enhance advance options toggle container

--- src/components/common/ToggleContainer/cmp.tsx
+++ src/components/common/ToggleContainer/cmp.tsx
@@ -5,28 +5,46 @@ import {
 } from '@aleph-front/core'
 import { ToggleContainerProps } from './types'
 import { ChangeEvent, memo, useCallback, useState } from 'react'
+import { StyledIcon } from './styles'
 
 export const ToggleContainer = ({
   label,
   children,
   ...rest
 }: ToggleContainerProps) => {
+  const [active, setActive] = useState(false)
   const [open, setOpen] = useState(false)
 
-  const handleChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => setOpen(event.target.checked),
-    [setOpen],
+  const handleChangeActive = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const active = event.target.checked
+
+      setActive(active)
+      setOpen(active)
+    },
+    [setActive, setOpen],
   )
+
+  const handleClickOpen = useCallback(() => setOpen((prev) => !prev), [setOpen])
 
   return (
     <NoisyContainer {...rest}>
-      <Switch
-        label={label}
-        onChange={handleChange}
-        checked={open}
-        className="tp-body3"
-      />
-      <CoreToggleContainer open={open} shouldUnmount>
+      <div tw="flex justify-between items-center">
+        <Switch
+          label={label}
+          onChange={handleChangeActive}
+          checked={active}
+          className={'tp-body3 ' + (active ? ' text-main0' : '')}
+        />
+        <StyledIcon
+          {...{
+            $isOpen: open,
+            $isVisible: active,
+            onClick: handleClickOpen,
+          }}
+        />
+      </div>
+      <CoreToggleContainer open={open} shouldUnmount={!active}>
         <div tw="pt-10 pb-6">{children}</div>
       </CoreToggleContainer>
     </NoisyContainer>
