3e1284a65e6ff11a0c3f13c331ebc5e163b80e5f
gmolki <gerardmo98@gmail.com>
2024-07-07 10:14:30 +0200

    fix: dashboard entity cards styles

--- src/components/pages/dashboard/DashboardPage/cmp.tsx
+++ src/components/pages/dashboard/DashboardPage/cmp.tsx
@@ -6,6 +6,7 @@ import Container from '@/components/common/CenteredContainer'
 import EntityCard from '@/components/common/EntityCard'
 import { HoldTokenDisclaimer } from '@/components/common/HoldTokenDisclaimer/cmp'
 import { EntityCardItemProps } from '@/components/common/EntityCard/types'
+import { EntityCardWrapper, StyledEntityCardsContainer } from './styles'
 
 export default function DashboardPage() {
   const {
@@ -72,81 +73,94 @@ export default function DashboardPage() {
           <SectionTitle number="1">
             Web3 Hosting <Icon name="globe" color="main0" size="0.66em" />
           </SectionTitle>
-          <div tw="mt-3 flex gap-6 items-stretch flex-wrap">
-            <EntityCard
-              type={
-                programAggregatedStatus.total.amount ? 'active' : 'introduction'
-              }
-              title="Websites"
-              img="Object8"
-              description="Deploy and manage your websites with ease. Support for static pages, Next.js, React, and Vue.js ensures you can create dynamic and robust web experiences."
-              introductionButtonText="Deploy your website"
-              dashboardPath="/hosting/website"
-              createPath="/hosting/website/new"
-              information={{
-                type: 'amount',
-                data: websitesAggregatedStatus.total,
-              }}
-            />
-          </div>
+          <StyledEntityCardsContainer tw="mt-3">
+            <EntityCardWrapper>
+              <EntityCard
+                tw="overflow-scroll"
+                type={
+                  programAggregatedStatus.total.amount
+                    ? 'active'
+                    : 'introduction'
+                }
+                title="Websites"
+                img="Object8"
+                description="Deploy and manage your websites with ease. Support for static pages, Next.js, React, and Vue.js ensures you can create dynamic and robust web experiences."
+                introductionButtonText="Deploy your website"
+                dashboardPath="/hosting/website"
+                createPath="/hosting/website/new"
+                information={{
+                  type: 'amount',
+                  data: websitesAggregatedStatus.total,
+                }}
+              />
+            </EntityCardWrapper>
+          </StyledEntityCardsContainer>
         </section>
         <section tw="px-0 pt-20 pb-6 md:py-10">
           <SectionTitle number="2">
             Computing <Icon name="settings" color="main0" size="0.66em" />
           </SectionTitle>
-          <div tw="mt-3 flex gap-6 items-stretch flex-wrap">
-            <EntityCard
-              type={
-                programAggregatedStatus.total.amount ? 'active' : 'introduction'
-              }
-              title="functions"
-              img="Object10"
-              description="Run code on-demand or persistently with our serverless computing platform. Scale effortlessly and integrate seamlessly."
-              introductionButtonText="Create your function"
-              dashboardPath="/computing/function"
-              createPath="/computing/function/new"
-              information={{
-                type: 'amount',
-                data: programAggregatedStatus.total,
-              }}
-              subItems={programsCardItems}
-            />
-            <EntityCard
-              type={
-                instanceAggregatedStatus.total.amount
-                  ? 'active'
-                  : 'introduction'
-              }
-              title="instance"
-              img="Object11"
-              description="Launch and manage virtual private servers (VPS) tailored to your needs. Choose automatic or manual node selection for complete control over your computing environment."
-              introductionButtonText="Create your instance"
-              dashboardPath="/computing/instance"
-              createPath="/computing/instance/new"
-              information={{
-                type: 'computing',
-                data: instanceAggregatedStatus.total,
-              }}
-            />
-            <EntityCard
-              type={'introduction'}
-              isComingSoon
-              title="confidentials"
-              img="Object9"
-              description="Protect your sensitive workloads with our Confidential VMs. Designed for maximum privacy and security, ensuring your data stays safe."
-              introductionButtonText="Create your confidential"
-              information={{
-                type: 'computing',
-              }}
-            />
-          </div>
+          <StyledEntityCardsContainer tw="mt-3">
+            <EntityCardWrapper>
+              <EntityCard
+                type={
+                  programAggregatedStatus.total.amount
+                    ? 'active'
+                    : 'introduction'
+                }
+                title="functions"
+                img="Object10"
+                description="Run code on-demand or persistently with our serverless computing platform. Scale effortlessly and integrate seamlessly."
+                introductionButtonText="Create your function"
+                dashboardPath="/computing/function"
+                createPath="/computing/function/new"
+                information={{
+                  type: 'amount',
+                  data: programAggregatedStatus.total,
+                }}
+                subItems={programsCardItems}
+              />
+            </EntityCardWrapper>
+            <EntityCardWrapper>
+              <EntityCard
+                type={
+                  instanceAggregatedStatus.total.amount
+                    ? 'active'
+                    : 'introduction'
+                }
+                title="instance"
+                img="Object11"
+                description="Launch and manage virtual private servers (VPS) tailored to your needs. Choose automatic or manual node selection for complete control over your computing environment."
+                introductionButtonText="Create your instance"
+                dashboardPath="/computing/instance"
+                createPath="/computing/instance/new"
+                information={{
+                  type: 'computing',
+                  data: instanceAggregatedStatus.total,
+                }}
+              />
+            </EntityCardWrapper>
+            <EntityCardWrapper>
+              <EntityCard
+                type={'introduction'}
+                isComingSoon
+                title="confidentials"
+                img="Object9"
+                description="Protect your sensitive workloads with our Confidential VMs. Designed for maximum privacy and security, ensuring your data stays safe."
+                introductionButtonText="Create your confidential"
+                information={{
+                  type: 'computing',
+                }}
+              />
+            </EntityCardWrapper>
+          </StyledEntityCardsContainer>
         </section>
         <section tw="px-0 pt-20 pb-6 md:py-10">
           <SectionTitle number="3">
             Storage <Icon name="folders" color="main0" size="0.66em" />
           </SectionTitle>
-          <div tw="flex mt-6">
-            <div tw="flex flex-wrap gap-6 items-stretch">
+          <StyledEntityCardsContainer tw="mt-6">
+            <EntityCardWrapper>
               <EntityCard
                 type={
                   volumesAggregatedStatus.total.amount
@@ -156,7 +170,7 @@ export default function DashboardPage() {
                 title="volumes"
                 img="Object15"
                 dashboardPath="/storage"
-                createPath="/storage/new"
+                createPath="/storage/volume/new"
                 description="Secure and reliable immutable volumes for your data storage needs. Ideal for dependency volumes and critical data, ensuring consistency and integrity."
                 introductionButtonText="Create your volume"
                 information={{
@@ -165,8 +179,8 @@ export default function DashboardPage() {
                 }}
                 subItems={volumesCardItems}
               />
-            </div>
-          </div>
+            </EntityCardWrapper>
+          </StyledEntityCardsContainer>
         </section>
       </Container>
       <Container $variant="xl">
