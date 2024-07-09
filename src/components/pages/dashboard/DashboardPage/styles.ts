02cefc694ffbf14604980d9087a286ffa3245932
gmolki <gerardmo98@gmail.com>
2024-07-09 12:14:51 +0200

    refactor(style): hide scrollbar for dashboard entity card wrapper

--- src/components/pages/dashboard/DashboardPage/styles.ts
+++ src/components/pages/dashboard/DashboardPage/styles.ts
@@ -7,6 +7,15 @@ export const StyledEntityCardsContainer = styled.div`
 
 export const EntityCardWrapper = styled.div`
   ${tw`flex overflow-x-scroll`}
+
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 `
 
 export const StyledSectionDescription = styled.p`
