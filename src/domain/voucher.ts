de38b71b07b85b5312d25a7592043d8cc9efda11
gmolki <gerardmo98@gmail.com>
2024-08-13 17:51:34 +0200

    refactor: voucher model structure

--- src/domain/voucher.ts
+++ src/domain/voucher.ts
@@ -5,11 +5,13 @@ import {
 } from '@aleph-sdk/client'
 
 export type Voucher = {
+  id: string
   metadataId: string
   name: string
   description: string
   externalUrl: string
   image: string
+  icon: string
   attributes: {
     value: string | number
     traitType: string
@@ -36,18 +38,23 @@ export class VoucherManager {
       for (const voucherId in content.nft_vouchers) {
         const voucher = content.nft_vouchers[voucherId]
 
-        if (voucher.claimer === this.account.address) {
+        if (voucher?.claimer === this.account.address) {
           const metadataId = voucher.metadata_id
           const metadata = content.metadata[metadataId]
 
           if (metadata) {
+            const { name, description, external_url, image, icon, attributes } =
+              metadata
+
             vouchers.push({
+              id: voucherId,
               metadataId,
-              name: metadata.name,
-              description: metadata.description,
-              externalUrl: metadata.external_url,
-              image: metadata.image,
-              attributes: metadata.attributes.map((attr: any) => ({
+              name,
+              description,
+              externalUrl: external_url.replace('{id}', voucherId),
+              image: image,
+              icon: icon,
+              attributes: attributes.map((attr: any) => ({
                 value: attr.value,
                 traitType: attr.trait_type,
                 displayType: attr.display_type,
