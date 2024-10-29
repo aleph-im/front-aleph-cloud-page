39cdeea6ef08ffa1ad26af02c9053bbdbd010765
gmolki <gerardmo98@gmail.com>
2024-10-29 17:36:09 +0100

    refactor: remove deprecated comment from ResponsiveTooltip

--- src/components/common/ResponsiveTooltip/styles.ts
+++ src/components/common/ResponsiveTooltip/styles.ts
@@ -18,12 +18,3 @@ export const StyledResponsiveTooltip = styled(
     css,
   }
 })<StyledResponsiveTooltipProps>``
-
-// <StyledResponsiveTooltipProps>`
-//   ${({ $isMobile }) => css`
-//     ${$isMobile &&
-//     tw`!absolute !top-0 !left-0 !transform-none !z-50 !w-[calc(100% - 3rem)] !h-[calc(100% - 3rem)] !max-w-full !text-red-50 !bg-red-500`}
-
-//     ${tw`!p-12`}
-//   `}
-// `
