17895b8fc3d84266da8bbd2ce08e09c928138c43
Gerard Molina <47140788+gmolki@users.noreply.github.com>
2024-11-06 12:32:08 +0100

    fix: update python runtime (#106)
    * fix: update runtimes
    
    * deps: upgrade sdk message

--- src/domain/runtime.ts
+++ src/domain/runtime.ts
@@ -1,7 +1,7 @@
 export enum FunctionRuntimeId {
-  Runtime1 = 'bd79839bf96e595a06da5ac0b6ba51dea6f7e2591bb913deccded04d831d29f4',
+  Runtime1 = '63f07193e6ee9d207b7d1fcf8286f9aee34e6f12f101d2ec77c1229f92964696',
   // @note: Added trailing blank spaces for generating different unique ids (it will be safely .trim() before sending the request) until the right hashes are provided
-  Runtime2 = 'bd79839bf96e595a06da5ac0b6ba51dea6f7e2591bb913deccded04d831d29f4 ',
+  Runtime2 = '63f07193e6ee9d207b7d1fcf8286f9aee34e6f12f101d2ec77c1229f92964696 ',
   // @note: old nvm runtime
   // Runtime3 = 'a14560e617f24338517902599a019890fc265425f3311d29b56c7e7603defc32',
   Runtime3 = '3c238dd3ffba73ab9b2cccb90a11e40e78aff396152de922a6d794a0a65a305e',
@@ -17,7 +17,7 @@ export type FunctionRuntime = {
 export const FunctionRuntimes: Record<FunctionRuntimeId, FunctionRuntime> = {
   [FunctionRuntimeId.Runtime1]: {
     id: FunctionRuntimeId.Runtime1,
-    name: 'Official runtime with Debian 11, Python 3.9 & Node.js 14',
+    name: 'Official runtime with Debian 12, Python 3.12 & Node.js 14',
     dist: 'debian',
   },
   [FunctionRuntimeId.Runtime2]: {
