93365ecf96b992aff00705710400651c9461d8e3
gmolki <gerardmo98@gmail.com>
2024-11-25 11:09:54 +0100

    feat: show solana vouchers

--- src/domain/voucher.ts
+++ src/domain/voucher.ts
@@ -1,8 +1,16 @@
+import axios from 'axios'
 import { Account } from '@aleph-sdk/account'
 import {
   AlephHttpClient,
   AuthenticatedAlephHttpClient,
 } from '@aleph-sdk/client'
+import { Blockchain } from '@aleph-sdk/core'
+
+export type VoucherAttribute = {
+  value: string | number
+  traitType: string
+  displayType?: string
+}
 
 export type Voucher = {
   id: string
@@ -12,11 +20,7 @@ export type Voucher = {
   externalUrl: string
   image: string
   icon: string
-  attributes: {
-    value: string | number
-    traitType: string
-    displayType?: string
-  }
+  attributes: VoucherAttribute[]
 }
 
 export class VoucherManager {
@@ -29,44 +33,111 @@ export class VoucherManager {
     if (!this.account) return []
 
     try {
-      const { content } = await this.sdkClient.getPost({
-        types: 'vouchers-update',
-      })
+      if (this.account.getChain() === Blockchain.SOL) {
+        return await this.fetchSolanaVouchers()
+      } else {
+        return await this.fetchEVMVouchers()
+      }
+    } catch (error) {
+      console.error('Error fetching vouchers:', error)
+      return []
+    }
+  }
 
-      const vouchers: Voucher[] = []
+  private async fetchSolanaVouchers(): Promise<Voucher[]> {
+    const vouchers: Voucher[] = []
+    const apiUrl = 'https://api.claim.twentysix.cloud/v1/registry/sol'
 
-      for (const voucherId in content.nft_vouchers) {
-        const voucher = content.nft_vouchers[voucherId]
+    try {
+      const response = await axios.get(apiUrl)
 
-        if (voucher?.claimer === this.account.address) {
-          const metadataId = voucher.metadata_id
-          const metadata = content.metadata[metadataId]
+      if (response.status !== 200) return vouchers
+
+      const { claimed_tickets: claimedTickets, batches } = response.data
+
+      for (const voucherId in claimedTickets) {
+        const { claimer, batch_id: batchId } = claimedTickets[voucherId]
+
+        if (claimer === this.account.address) {
+          const { metadata_id: metadataId } = batches[batchId]
+          const metadata = await this.fetchMetadata(metadataId)
 
           if (metadata) {
-            const { name, description, external_url, image, icon, attributes } =
-              metadata
-
-            vouchers.push({
-              id: voucherId,
-              metadataId,
-              name,
-              description,
-              externalUrl: external_url.replace('{id}', voucherId),
-              image: image,
-              icon: icon,
-              attributes: attributes.map((attr: any) => ({
-                value: attr.value,
-                traitType: attr.trait_type,
-                displayType: attr.display_type,
-              })),
-            })
+            vouchers.push(this.createVoucher(voucherId, metadataId, metadata))
           }
         }
       }
 
       return vouchers
-    } catch (err) {
-      return []
+    } catch (error) {
+      console.error('Error fetching Solana vouchers:', error)
+      return vouchers
+    }
+  }
+
+  private async fetchEVMVouchers(): Promise<Voucher[]> {
+    const vouchers: Voucher[] = []
+    const { content } = await this.sdkClient.getPost({
+      types: 'vouchers-update',
+    })
+
+    for (const voucherId in content.nft_vouchers) {
+      const voucher = content.nft_vouchers[voucherId]
+
+      if (voucher?.claimer === this.account.address) {
+        const metadataId = voucher.metadata_id
+        const metadata = content.metadata[metadataId]
+
+        if (metadata) {
+          vouchers.push(this.createVoucher(voucherId, metadataId, metadata))
+        }
+      }
+    }
+
+    return vouchers
+  }
+
+  private async fetchMetadata(metadataId: string) {
+    try {
+      const metadataUrl = `https://claim.twentysix.cloud/sbt/metadata/${metadataId}.json`
+      const response = await axios.get(metadataUrl)
+
+      if (response.status !== 200) return null
+
+      return response.data
+    } catch (error) {
+      console.error('Error fetching metadata:', error)
+      return null
+    }
+  }
+
+  private createVoucher(
+    voucherId: string,
+    metadataId: string,
+    metadata: any,
+  ): Voucher {
+    const {
+      name,
+      description,
+      external_url: externalUrl,
+      image,
+      icon,
+      attributes,
+    } = metadata
+
+    return {
+      id: voucherId,
+      metadataId,
+      name,
+      description,
+      externalUrl: externalUrl.replace('{id}', voucherId),
+      image,
+      icon,
+      attributes: attributes.map((attr: any) => ({
+        value: attr.value,
+        traitType: attr.trait_type,
+        displayType: attr.display_type,
+      })),
     }
   }
 }
