58a0ecb893c2858f723ea4f7ac5ceed7e381f0c4
gmolki <gerardmo98@gmail.com>
2025-01-13 17:42:05 +0100

    refactor: loading state

---
+++ src/components/common/Skeleton/styles.tsx
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components'
+import tw from 'twin.macro'
+import { StyledSkeletonProps } from './types'
+
+export const StyledSkeleton = styled.div<StyledSkeletonProps>`
+  ${tw`animate-pulse rounded-md`}
+
+  ${({ theme, $width, $height }) => css`
+    background: ${theme.color.purple4};
+    width: ${$width};
+    height: ${$height};
+  `}
+`
