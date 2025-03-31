162408b25d4e2f5f073b8026b1c8e90b69b68bb4
gmolki <gerardmo98@gmail.com>
2025-03-31 11:42:31 +0200

    fix(SSHKeyDetail): loading state

--- src/components/common/SSHKeyDetail/cmp.tsx
+++ src/components/common/SSHKeyDetail/cmp.tsx
@@ -7,24 +7,32 @@ import { Separator, Text } from '@/components/pages/dashboard/common'
 import { ellipseAddress, ellipseText } from '@/helpers/utils'
 import { useSSHKeyDetail } from './hook'
 import { EntityTypeName } from '@/helpers/constants'
+import Skeleton from '../Skeleton'
 
 export const SSHKeyDetail = ({ sshKeyId }: SSHKeyDetailProps) => {
   const { sshKey, theme, handleCopyKey, handleCopyLabel, handleDelete } =
     useSSHKeyDetail({ sshKeyId })
-
-  if (!sshKey) return <>NO SSH KEY</>
-
-  const name = sshKey.label || ellipseAddress(sshKey.id)
-  const typeName = EntityTypeName[sshKey.type]
 
   return (
     <>
       <div tw="flex justify-between pb-5">
         <div tw="flex items-center">
           <Icon name="key" tw="mr-4" className="text-main0" />
-          <div className="tp-body2">{name}</div>
-          <Label variant={sshKey.confirmed ? 'success' : 'warning'} tw="ml-4">
-            {sshKey.confirmed ? (
+          <div className="tp-body2">
+            {sshKey ? (
+              sshKey.label || ellipseAddress(sshKey.id)
+            ) : (
+              <Skeleton width="10rem" />
+            )}
+          </div>
+          <Label
+            kind="secondary"
+            variant={sshKey?.confirmed ? 'success' : 'warning'}
+            tw="ml-4"
+          >
+            {!sshKey ? (
+              'LOADING'
+            ) : sshKey.confirmed ? (
               'READY'
             ) : (
               <div tw="flex items-center">
@@ -49,24 +57,28 @@ export const SSHKeyDetail = ({ sshKeyId }: SSHKeyDetailProps) => {
       <NoisyContainer>
         <div tw="flex items-center justify-start overflow-hidden">
           <Tag variant="accent" tw="mr-4 whitespace-nowrap">
-            {typeName}
+            {sshKey ? (
+              EntityTypeName[sshKey.type]
+            ) : (
+              <Skeleton width="5rem" height="1.3rem" />
+            )}
           </Tag>
           <div tw="flex-auto">
             <div className="tp-info text-main0">KEY</div>
             <IconText iconName="copy" onClick={handleCopyKey}>
-              {sshKey.key}
+              {sshKey ? sshKey.key : <Skeleton width="12rem" />}
             </IconText>
           </div>
         </div>
 
         <Separator />
 
-        {sshKey.label && (
+        {sshKey?.label && (
           <div tw="my-5">
             <div className="tp-info text-main0">LABEL</div>
             <div>
               <IconText iconName="copy" onClick={handleCopyLabel}>
-                <Text>{sshKey.label}</Text>
+                <Text>{sshKey?.label}</Text>
               </IconText>
             </div>
           </div>
@@ -75,23 +87,29 @@ export const SSHKeyDetail = ({ sshKeyId }: SSHKeyDetailProps) => {
         <div tw="my-5">
           <div className="tp-info text-main0">EXPLORER</div>
           <div>
-            <a
-              className="tp-body1 fs-16"
-              href={sshKey.url}
-              target="_blank"
-              referrerPolicy="no-referrer"
-            >
-              <IconText iconName="square-up-right">
-                <Text>{ellipseText(sshKey.url, 80)}</Text>
-              </IconText>
-            </a>
+            {sshKey ? (
+              <a
+                className="tp-body1 fs-16"
+                href={sshKey.url}
+                target="_blank"
+                referrerPolicy="no-referrer"
+              >
+                <IconText iconName="square-up-right">
+                  <Text>{ellipseText(sshKey.url, 80)}</Text>
+                </IconText>
+              </a>
+            ) : (
+              <Skeleton width="10rem" />
+            )}
           </div>
         </div>
 
         <div tw="my-5">
           <div className="tp-info text-main0">CREATED ON</div>
           <div>
-            <Text className="fs-10 tp-body1">{sshKey.date}</Text>
+            <Text className="fs-10 tp-body1">
+              {sshKey ? sshKey.date : <Skeleton width="4rem" />}
+            </Text>
           </div>
         </div>
       </NoisyContainer>
