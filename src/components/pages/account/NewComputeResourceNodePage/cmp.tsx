243923838dabd5b9b8017c8c6a7ef63fffc91d3c
gmolki <gerardmo98@gmail.com>
2025-04-03 17:26:10 +0200

    refactor: styles for account form pages

--- src/components/pages/account/NewComputeResourceNodePage/cmp.tsx
+++ src/components/pages/account/NewComputeResourceNodePage/cmp.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   CompositeTitle,
   NoisyContainer,
+  TextGradient,
   TextInput,
 } from '@aleph-front/core'
 import { useNewComputeResourceNodePage } from './hook'
@@ -25,71 +26,76 @@ export const NewComputeResourceNodePage = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <section>
-        <h1 className="tp-h5" tw="mb-8">
-          Create compute resource node
-        </h1>
-      </section>
-      <Form onSubmit={handleSubmit} errors={errors} tw="max-w-[44.6875rem]">
-        <section tw="px-0 pb-6 md:pb-10">
-          <CompositeTitle as="h2" number="1">
-            Set a name
-          </CompositeTitle>
-          <p tw="mt-1 mb-6">Your node name should be short and meaningful.</p>
-          <NoisyContainer>
-            <TextInput
-              {...nameCtrl.field}
-              {...nameCtrl.fieldState}
-              required
-              label="Node name"
-              placeholder="Give it a name"
-            />
-            <p tw="mt-4">
-              You can change this later if you want, even add a picture and
-              description.
-            </p>
-          </NoisyContainer>
-        </section>
-        <section tw="px-0 py-6 md:py-10">
-          <CompositeTitle as="h2" number="2">
-            Set address
-          </CompositeTitle>
-          <p tw="mt-1 mb-6">
-            The physical node has a unique identifier address. It links the
-            wallet and interface to the physical node. This address is mandatory
-            to proceed and the install procedure below will explain how to
-            retrieve this address.
-          </p>
-          <NoisyContainer>
-            <TextInput
-              {...addressCtrl.field}
-              {...addressCtrl.fieldState}
-              required
-              label="Set address"
-              placeholder="https://my-domain.tld/"
-            />
-            <div tw="mt-2">
-              <ExternalLinkButton href="https://docs.aleph.im/nodes/compute/">
-                How to install your node and retrieve your address
-              </ExternalLinkButton>
+
+      <div tw="px-6 lg:px-16 pb-6 lg:pb-5">
+        <section>
+          <TextGradient type="h5" forwardedAs="h1" tw="mb-8">
+            Create compute resource node
+          </TextGradient>
+          <Form onSubmit={handleSubmit} errors={errors} tw="max-w-[44.6875rem]">
+            <div tw="px-0 pb-6 md:pb-10">
+              <CompositeTitle as="h2" number="1" color="main0">
+                Set a name
+              </CompositeTitle>
+              <p tw="mt-1 mb-6">
+                Your node name should be short and meaningful.
+              </p>
+              <NoisyContainer>
+                <TextInput
+                  {...nameCtrl.field}
+                  {...nameCtrl.fieldState}
+                  required
+                  label="Node name"
+                  placeholder="Give it a name"
+                />
+                <p tw="mt-4">
+                  You can change this later if you want, even add a picture and
+                  description.
+                </p>
+              </NoisyContainer>
             </div>
-          </NoisyContainer>
+            <div tw="px-0 py-6 md:pb-10">
+              <CompositeTitle as="h2" number="2" color="main0">
+                Set address
+              </CompositeTitle>
+              <p tw="mt-1 mb-6">
+                The physical node has a unique identifier address. It links the
+                wallet and interface to the physical node. This address is
+                mandatory to proceed and the install procedure below will
+                explain how to retrieve this address.
+              </p>
+              <NoisyContainer>
+                <TextInput
+                  {...addressCtrl.field}
+                  {...addressCtrl.fieldState}
+                  required
+                  label="Set address"
+                  placeholder="https://my-domain.tld/"
+                />
+                <div tw="mt-2">
+                  <ExternalLinkButton href="https://docs.aleph.im/nodes/compute/">
+                    How to install your node and retrieve your address
+                  </ExternalLinkButton>
+                </div>
+              </NoisyContainer>
+            </div>
+            <div tw="flex gap-10">
+              <Button
+                color="main0"
+                kind="gradient"
+                variant="primary"
+                size="md"
+                type="submit"
+              >
+                Register compute node
+              </Button>
+              <ButtonLink size="md" variant="textOnly" href="/account/earn/crn">
+                Cancel
+              </ButtonLink>
+            </div>
+          </Form>
         </section>
-        <section tw="flex gap-10">
-          <Button
-            color="main0"
-            kind="gradient"
-            variant="primary"
-            size="md"
-            type="submit"
-          >
-            Register compute node
-          </Button>
-          <ButtonLink size="md" variant="textOnly" href="/account/earn/crn">
-            Cancel
-          </ButtonLink>
-        </section>
-      </Form>
+      </div>
     </>
   )
 }
--- src/components/pages/account/NewCoreChannelNodePage/cmp.tsx
+++ src/components/pages/account/NewCoreChannelNodePage/cmp.tsx
@@ -5,6 +5,7 @@ import {
   Button,
   CompositeTitle,
   NoisyContainer,
+  TextGradient,
   TextInput,
 } from '@aleph-front/core'
 import { useNewCoreChannelNodePage } from './hook'
@@ -25,72 +26,76 @@ export const NewCoreChannelNodePage = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <section>
-        <h1 className="tp-h5" tw="mb-8">
-          Create core node
-        </h1>
-      </section>
-      <Form onSubmit={handleSubmit} errors={errors} tw="max-w-[44.6875rem]">
-        <section tw="px-0 pb-6 md:pb-10">
-          <CompositeTitle as="h2" number="1">
-            Set a name
-          </CompositeTitle>
-          <p tw="mt-1 mb-6">Your node name should be short and meaningful.</p>
-          <NoisyContainer>
-            <TextInput
-              {...nameCtrl.field}
-              {...nameCtrl.fieldState}
-              required
-              label="Node name"
-              placeholder="Give it a name"
-            />
-            <p tw="mt-4">
-              You can change this later if you want, even add a picture and
-              description.
-            </p>
-          </NoisyContainer>
-        </section>
-        <section tw="px-0 py-6 md:py-10">
-          <CompositeTitle as="h2" number="2">
-            Set multiaddress
-          </CompositeTitle>
-          <p tw="mt-1 mb-6">
-            The physical node has unique identifier called multiaddress. It
-            links the physical node with the interface. Can be setup later on
-            the info page of the node or here if the physical node is running.
-            The link below explains in detail the install procedure and how to
-            retrieve the multiaddress.
-          </p>
-          <NoisyContainer>
-            <TextInput
-              {...multiaddressCtrl.field}
-              {...multiaddressCtrl.fieldState}
-              required
-              label="Set multiaddress"
-              placeholder="/ip4/{ip-address}/tcp/4025/p2p/SmlS3hb..."
-            />
-            <div tw="mt-2">
-              <ExternalLinkButton href="https://docs.aleph.im/nodes/core/">
-                How to create a node
-              </ExternalLinkButton>
-            </div>
-          </NoisyContainer>
-        </section>
-        <section tw="flex gap-10">
-          <Button
-            color="main0"
-            kind="gradient"
-            variant="primary"
-            size="md"
-            type="submit"
-          >
+      <div tw="px-6 lg:px-16 pb-6 lg:pb-5">
+        <section>
+          <TextGradient type="h5" forwardedAs="h1" tw="mb-8">
             Create core node
-          </Button>
-          <ButtonLink size="md" variant="textOnly" href="/account/earn/ccn">
-            Cancel
-          </ButtonLink>
+          </TextGradient>
+          <Form onSubmit={handleSubmit} errors={errors} tw="max-w-[44.6875rem]">
+            <section tw="px-0 pb-6 md:pb-10">
+              <CompositeTitle as="h2" number="1" color="main0">
+                Set a name
+              </CompositeTitle>
+              <p tw="mt-1 mb-6">
+                Your node name should be short and meaningful.
+              </p>
+              <NoisyContainer>
+                <TextInput
+                  {...nameCtrl.field}
+                  {...nameCtrl.fieldState}
+                  required
+                  label="Node name"
+                  placeholder="Give it a name"
+                />
+                <p tw="mt-4">
+                  You can change this later if you want, even add a picture and
+                  description.
+                </p>
+              </NoisyContainer>
+            </section>
+            <section tw="px-0 py-6 md:py-10">
+              <CompositeTitle as="h2" number="2" color="main0">
+                Set multiaddress
+              </CompositeTitle>
+              <p tw="mt-1 mb-6">
+                The physical node has unique identifier called multiaddress. It
+                links the physical node with the interface. Can be setup later
+                on the info page of the node or here if the physical node is
+                running. The link below explains in detail the install procedure
+                and how to retrieve the multiaddress.
+              </p>
+              <NoisyContainer>
+                <TextInput
+                  {...multiaddressCtrl.field}
+                  {...multiaddressCtrl.fieldState}
+                  required
+                  label="Set multiaddress"
+                  placeholder="/ip4/{ip-address}/tcp/4025/p2p/SmlS3hb..."
+                />
+                <div tw="mt-2">
+                  <ExternalLinkButton href="https://docs.aleph.im/nodes/core/">
+                    How to create a node
+                  </ExternalLinkButton>
+                </div>
+              </NoisyContainer>
+            </section>
+            <section tw="flex gap-10">
+              <Button
+                color="main0"
+                kind="gradient"
+                variant="primary"
+                size="md"
+                type="submit"
+              >
+                Create core node
+              </Button>
+              <ButtonLink size="md" variant="textOnly" href="/account/earn/ccn">
+                Cancel
+              </ButtonLink>
+            </section>
+          </Form>
         </section>
-      </Form>
+      </div>
     </>
   )
 }
