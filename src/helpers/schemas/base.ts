6c53df5e1ea47a62a1d98b493543437fea5c77ee
amalcaraz <amalcaraz89@gmail.com>
2025-04-08 12:43:18 +0200

    fix: update domain validation regex to allow multi-level subdomains

--- src/helpers/schemas/base.ts
+++ src/helpers/schemas/base.ts
@@ -38,7 +38,7 @@ export const messageHashSchema = requiredStringSchema.regex(/^[0-9a-f]{64}$/, {
 // /^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$/
 
 export const domainNameSchema = requiredStringSchema.regex(
-  /^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/,
+  /^((?!-))(xn--)?([a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.)+((xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,}))$/,
   { message: 'Invalid domain format' },
 )
 
