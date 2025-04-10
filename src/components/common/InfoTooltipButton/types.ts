5e3b324700291b3079330a2219780cd4107e68bc
gmolki <gerardmo98@gmail.com>
2025-04-10 17:22:45 +0200

    refactor(SelectInstanceSpecs): show disabled reason

--- src/components/common/InfoTooltipButton/types.ts
+++ src/components/common/InfoTooltipButton/types.ts
@@ -2,7 +2,7 @@ import { IconSize, TooltipProps } from '@aleph-front/core'
 import { ReactNode } from 'react'
 
 export type InfoTooltipButtonProps = Omit<TooltipProps, 'targetRef'> & {
-  children: ReactNode
+  children?: ReactNode
   tooltipContent: ReactNode
   plain?: boolean
   align?: 'left' | 'right'
