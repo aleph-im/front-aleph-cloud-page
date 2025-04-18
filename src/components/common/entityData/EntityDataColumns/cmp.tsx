e78b1d628a57cd7d667f00ddd938ef62e46c9d16
gmolki <gerardmo98@gmail.com>
2025-04-18 20:57:11 +0200

    refactor: create EntityDataColumns cmp

---
+++ src/components/common/entityData/EntityDataColumns/cmp.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react'
+import { EntityDataColumnsProps } from './types'
+
+export const EntityDataColumns = ({
+  leftColumnElements,
+  rightColumnElements,
+}: EntityDataColumnsProps) => {
+  const renderElements = (elements: React.ReactNode[]) =>
+    elements
+      .filter(Boolean)
+      .map((element, index) => <div key={index}>{element}</div>)
+
+  return (
+    <div tw="w-full flex flex-wrap gap-x-24 gap-y-9 px-12 py-6">
+      <div tw="flex-1 w-1/2 flex flex-col gap-y-9">
+        {renderElements(leftColumnElements)}
+      </div>
+      <div tw="flex-1 w-1/2 flex flex-col gap-y-9">
+        {renderElements(rightColumnElements)}
+      </div>
+    </div>
+  )
+}
+
+EntityDataColumns.displayName = 'EntityDataColumns'
+
+export default memo(EntityDataColumns) as typeof EntityDataColumns
