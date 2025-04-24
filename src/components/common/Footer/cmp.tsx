4172beda8dc7292a3de9397ffd55d18a8f13381b
gmolki <gerardmo98@gmail.com>
2025-04-24 09:08:14 +0200

    fix: small footer props

--- src/components/common/Footer/cmp.tsx
+++ src/components/common/Footer/cmp.tsx
@@ -5,47 +5,47 @@ import Link from 'next/link'
 export const Footer = () => {
   return (
     <BaseFooter
-      {...{
-        small: true,
-        maxWidth: '100%',
-        Link: Link as LinkComponent,
-        logoHref: websiteUrl,
-        logoTarget: '_blank',
-        logoImg: 'aleph',
-        logoByAleph: false,
-        logoText: 'Aleph Cloud',
-        breakpoint: 'lg',
-        media: [
-          {
-            name: 'x',
-            icon: 'x',
-            label: 'Follow us',
-            href: 'https://x.com/aleph_im',
-            target: '_blank',
-            small: true,
-          },
-          {
-            name: 'medium',
-            icon: 'medium',
-            label: 'Medium',
-            href: 'https://medium.com/aleph-im',
-            target: '_blank',
-            small: true,
-          },
-        ],
-        mainLinks: [
-          {
-            label: 'Documentation',
-            href: 'https://docs.aleph.cloud',
-            target: '_blank',
-          },
-          {
-            label: 'Telegram Developers',
-            href: 'https://t.me/alephim/119590',
-            target: '_blank',
-          },
-        ],
-      }}
+      small
+      maxWidth="100%"
+      Link={Link as LinkComponent}
+      logoHref={websiteUrl}
+      logoTarget="_blank"
+      logoImg="aleph"
+      logoByAleph={false}
+      logoText="Aleph Cloud"
+      breakpoint="lg"
+      media={[
+        {
+          name: 'x',
+          icon: 'x',
+          label: 'Follow us',
+          href: 'https://x.com/aleph_im',
+          target: '_blank',
+          small: true,
+        },
+        {
+          name: 'medium',
+          icon: 'medium',
+          label: 'Medium',
+          href: 'https://medium.com/aleph-im',
+          target: '_blank',
+          small: true,
+        },
+      ]}
+      mainLinks={[
+        {
+          label: 'Documentation',
+          href: 'https://docs.aleph.cloud',
+          target: '_blank',
+        },
+        {
+          label: 'Telegram Developers',
+          href: 'https://t.me/alephim/119590',
+          target: '_blank',
+        },
+      ]}
+      buttons={[]}
+      links={[]}
     />
   )
 }
