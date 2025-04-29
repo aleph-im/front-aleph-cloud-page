72348971c594a84629b006bc6d9553b27209e72c
gmolki <gerardmo98@gmail.com>
2025-04-30 00:48:06 +0200

    feat: create EntityLinkedCodebase cmp

---
+++ src/components/common/entityData/EntityLinkedCodebase/cmp.tsx
@@ -0,0 +1,85 @@
+import React, { memo, useCallback } from 'react'
+import {
+  Icon,
+  NoisyContainer,
+  ObjectImg,
+  useCopyToClipboardAndNotify,
+} from '@aleph-front/core'
+import { EntityLinkedCodebaseProps } from './types'
+import RelatedEntityCard from '../RelatedEntityCard'
+import { FunctionalButton } from '@/components/pages/console/instance/ManageInstance/cmp'
+import Skeleton from '../../Skeleton'
+import { Text } from '@/components/pages/console/common'
+import { EntityType, EntityTypeObject } from '@/helpers/constants'
+
+export const EntityLinkedCodebase = ({
+  loading,
+  codebaseVolumeId,
+  entrypoint,
+  onCodebaseVolumeClick: handleCodebaseVolumeClick,
+}: EntityLinkedCodebaseProps) => {
+  const handleCopyVolumeHash = useCopyToClipboardAndNotify(
+    codebaseVolumeId || '',
+  )
+
+  const handleCopyHash = useCallback(
+    (e: any) => {
+      handleCopyVolumeHash()
+      e.preventDefault()
+    },
+    [handleCopyVolumeHash],
+  )
+
+  return (
+    <>
+      <div className="tp-h7 fs-24" tw="uppercase mb-2">
+        LINKED CODEBASE
+      </div>
+      <NoisyContainer>
+        <div tw="flex flex-col gap-4">
+          <div tw="flex flex-wrap items-center gap-4 -mt-2">
+            <RelatedEntityCard
+              disabled={!codebaseVolumeId}
+              onClick={() =>
+                codebaseVolumeId && handleCodebaseVolumeClick(codebaseVolumeId)
+              }
+            >
+              <ObjectImg
+                id={EntityTypeObject[EntityType.Volume]}
+                color="base2"
+                size="2.5rem"
+                tw="min-w-[3rem] min-h-[3rem]"
+              />
+              <div>
+                <div className="tp-info text-main0 fs-12">ENTRYPOINT</div>
+                <Text>
+                  {!entrypoint || loading ? (
+                    <Skeleton width="6rem" />
+                  ) : (
+                    entrypoint
+                  )}
+                </Text>
+              </div>
+
+              <Icon
+                name="eye"
+                tw="absolute top-2 right-2"
+                className="openEntityIcon"
+              />
+            </RelatedEntityCard>
+            <FunctionalButton
+              onClick={handleCopyHash}
+              disabled={!codebaseVolumeId}
+            >
+              <Icon name="copy" />
+              copy hash
+            </FunctionalButton>
+          </div>
+        </div>
+      </NoisyContainer>
+    </>
+  )
+}
+EntityLinkedCodebase.displayName = 'EntityLinkedCodebase'
+
+export default memo(EntityLinkedCodebase) as typeof EntityLinkedCodebase
