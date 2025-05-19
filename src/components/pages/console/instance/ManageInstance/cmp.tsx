42dd6f8a62a6234da820e6d5ba33cf9914effc32
gmolki <gerardmo98@gmail.com>
2025-05-19 18:18:35 +0200

    wip

--- src/components/pages/console/instance/ManageInstance/cmp.tsx
+++ src/components/pages/console/instance/ManageInstance/cmp.tsx
@@ -22,6 +22,7 @@ import ManageEntityHeader from '@/components/common/entityData/ManageEntityHeade
 import EntityDataColumns from '@/components/common/entityData/EntityDataColumns'
 import EntityCustomDomains from '@/components/common/entityData/EntityCustomDomains'
 import DomainDetail from '@/components/common/DomainDetail'
+import EntityPortForwarding from '@/components/common/entityData/EntityPortForwarding'
 
 /**
  * Button component with functional styling
@@ -190,6 +191,7 @@ export default function ManageInstance() {
                   onCustomDomainClick={handleCustomDomainClick}
                 />
               ),
+              <EntityPortForwarding />,
             ]}
           />
         </Slide>
