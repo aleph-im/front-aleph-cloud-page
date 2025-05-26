9a52c70e4bd44c01992a6ae1ef63be4ae50537e4
gmolki <gerardmo98@gmail.com>
2025-05-26 14:00:25 +0200

    refactor(EntityPortForwarding): component order

--- src/components/common/entityData/EntityPortForwarding/cmp.tsx
+++ src/components/common/entityData/EntityPortForwarding/cmp.tsx
@@ -21,44 +21,101 @@ export const EntityPortForwarding = ({}: EntityPortForwardingProps) => {
         IPV4 PORT FORWARDING
       </div>
       <NoisyContainer>
-        <div tw="flex flex-col flex-wrap gap-4">
-          <InfoTitle>ALLOWED PORTS</InfoTitle>
-          <Text>2222, 80, 443, and ports within the range 8006-8999</Text>
+        <div tw="flex flex-col gap-y-4">
+          <div>
+            <InfoTitle>CONFIGURED PORTS</InfoTitle>
+            <Text>Maximum 20 ports allowed</Text>
+          </div>
+          <div tw="flex gap-4 items-center">
+            <div tw="flex flex-col gap-3">
+              <Text>Source</Text>
+              {/* @todo: Remove min-width prop from TextInput on front-core */}
+              <TextInput
+                // {...nameCtrl.field}
+                // {...nameCtrl.fieldState}
+                dataView
+                defaultValue="80"
+                required
+              />
+            </div>
+            <div className="text-main0 fs-18" tw=" font-bold self-end mb-3">
+              {'-'}
+            </div>
+            <div tw="flex flex-col gap-3">
+              <Text>Destination</Text>
+              {/* @todo: Remove min-width prop from TextInput on front-core */}
+              <TextInput
+                // {...nameCtrl.field}
+                // {...nameCtrl.fieldState}
+                dataView
+                defaultValue="28743"
+                required
+              />
+            </div>
+            <div tw="flex flex-col gap-3">
+              <Text>UDP</Text>
+              <Checkbox disabled />
+            </div>
+            <div tw="flex flex-col gap-3">
+              <Text>TCP</Text>
+              <Checkbox disabled checked />
+            </div>
+            <div tw="self-end mb-1">
+              <Button
+                variant="warning"
+                kind="functional"
+                // onClick={handleRemovePort}
+              >
+                <Icon name="trash-xmark" />
+              </Button>
+            </div>
+          </div>
           <div>
             {showAddPort ? (
               <>
-                <div tw="flex gap-4 items-center">
-                  <div tw="flex flex-col gap-3">
-                    <Text>Port</Text>
-                    {/* @todo: Remove min-width prop from TextInput on front-core */}
-                    <TextInput
-                      // {...nameCtrl.field}
-                      // {...nameCtrl.fieldState}
-                      placeholder="8080"
-                      required
-                    />
+                <div tw="mt-2 px-4 py-3" className="bg-background">
+                  <div tw="flex gap-4 items-center">
+                    <div tw="flex flex-col gap-3">
+                      <Text>Port</Text>
+                      {/* @todo: Remove min-width prop from TextInput on front-core */}
+                      <TextInput
+                        // {...nameCtrl.field}
+                        // {...nameCtrl.fieldState}
+                        placeholder="8080"
+                        required
+                      />
+                    </div>
+                    <div tw="flex flex-col gap-3">
+                      <Text>UDP</Text>
+                      <Checkbox />
+                    </div>
+                    <div tw="flex flex-col gap-3">
+                      <Text>TCP</Text>
+                      <Checkbox />
+                    </div>
+                    <div tw="self-end mb-1">
+                      <Button
+                        variant="warning"
+                        kind="functional"
+                        onClick={handleCancelAddPort}
+                      >
+                        <Icon name="trash-xmark" />
+                      </Button>
+                    </div>
                   </div>
-                  <div tw="flex flex-col gap-3">
-                    <Text>UDP</Text>
-                    <Checkbox />
-                  </div>
-                  <div tw="flex flex-col gap-3">
-                    <Text>TCP</Text>
-                    <Checkbox />
-                  </div>
-                  <div tw="self-end mb-1">
+                  <div tw="mt-4">
                     <Button
-                      variant="warning"
-                      kind="functional"
-                      onClick={handleCancelAddPort}
+                      variant="secondary"
+                      kind="gradient"
+                      // onClick={handleAddNewPort}
                     >
-                      <Icon name="trash-xmark" />
+                      Add
                     </Button>
                   </div>
                 </div>
                 <div tw="mt-4">
                   <Button
-                    variant="secondary"
+                    variant="primary"
                     kind="gradient"
                     // onClick={handleSaveNewPort}
                   >
@@ -67,62 +124,16 @@ export const EntityPortForwarding = ({}: EntityPortForwardingProps) => {
                 </div>
               </>
             ) : (
-              <Button
-                variant="secondary"
-                kind="gradient"
-                onClick={handleAddPort}
-              >
-                Add
-              </Button>
-            )}
-          </div>
-          <div tw="mt-6">
-            <InfoTitle>CONFIGURED PORTS</InfoTitle>
-            <Text>Maximum 20 ports allowed</Text>
-            <div tw="flex gap-4 items-center">
-              <div tw="flex flex-col gap-3">
-                <Text>Source</Text>
-                {/* @todo: Remove min-width prop from TextInput on front-core */}
-                <TextInput
-                  // {...nameCtrl.field}
-                  // {...nameCtrl.fieldState}
-                  dataView
-                  defaultValue="80"
-                  required
-                />
-              </div>
-              <div className="text-main0 fs-18" tw=" font-bold self-end mb-3">
-                {'-'}
-              </div>
-              <div tw="flex flex-col gap-3">
-                <Text>Destination</Text>
-                {/* @todo: Remove min-width prop from TextInput on front-core */}
-                <TextInput
-                  // {...nameCtrl.field}
-                  // {...nameCtrl.fieldState}
-                  dataView
-                  defaultValue="28743"
-                  required
-                />
-              </div>
-              <div tw="flex flex-col gap-3">
-                <Text>UDP</Text>
-                <Checkbox disabled />
-              </div>
-              <div tw="flex flex-col gap-3">
-                <Text>TCP</Text>
-                <Checkbox disabled checked />
-              </div>
-              <div tw="self-end mb-1">
+              <div tw="mt-4">
                 <Button
-                  variant="warning"
-                  kind="functional"
-                  // onClick={handleRemovePort}
+                  variant="secondary"
+                  kind="gradient"
+                  onClick={handleAddPort}
                 >
-                  <Icon name="trash-xmark" />
+                  Add
                 </Button>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </NoisyContainer>
