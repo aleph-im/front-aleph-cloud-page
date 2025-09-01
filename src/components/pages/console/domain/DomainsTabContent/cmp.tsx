f350ae0040fc64abd05d8f5b95adfd73143cce79
Gerard Molina <47140788+gmolki@users.noreply.github.com>
2025-09-01 12:29:01 +0200

    refactor: entity table design (#172)
    * refactor: table styles
    
    * refactor: remove noisy container from tables bg

--- src/components/pages/console/domain/DomainsTabContent/cmp.tsx
+++ src/components/pages/console/domain/DomainsTabContent/cmp.tsx
@@ -3,7 +3,7 @@ import tw from 'twin.macro'
 import { DomainsTabContentProps } from './types'
 import ButtonLink from '@/components/common/ButtonLink'
 import EntityTable from '@/components/common/EntityTable'
-import { Icon, NoisyContainer } from '@aleph-front/core'
+import { Icon } from '@aleph-front/core'
 import IconText from '@/components/common/IconText'
 import { Text } from '../../common'
 import { EntityDomainType, EntityDomainTypeName } from '@/helpers/constants'
@@ -17,73 +17,71 @@ export const DomainsTabContent = ({
 
   return (
     <>
-      <NoisyContainer>
-        <div tw="overflow-auto max-w-full">
-          <EntityTable
-            borderType="none"
-            rowNoise
-            rowKey={(row) => row.id}
-            data={data}
-            rowProps={(row) => ({
-              css: row.confirmed ? '' : tw`opacity-60`,
-            })}
-            columns={[
-              {
-                label: 'Name',
-                width: '50%',
-                sortable: true,
-                render: (row) => (
-                  <a
-                    href={`https://${row.name}`}
-                    target="_blank"
-                    referrerPolicy="no-referrer"
-                  >
-                    <IconText iconName="square-up-right">
-                      <Text tw="not-italic! font-bold!">{row.name}</Text>
-                    </IconText>
-                  </a>
-                ),
-              },
-              {
-                label: 'Target',
-                sortable: true,
-                render: (row) => EntityDomainTypeName[row.target],
-              },
-              {
-                label: 'Ref',
-                width: '50%',
-                sortable: true,
-                render: (row) => {
-                  if (row.target === EntityDomainType.IPFS) {
-                    return row.ref
-                  }
+      <div tw="overflow-auto max-w-full">
+        <EntityTable
+          borderType="none"
+          rowNoise
+          rowKey={(row) => row.id}
+          data={data}
+          rowProps={(row) => ({
+            css: row.confirmed ? '' : tw`opacity-60`,
+          })}
+          columns={[
+            {
+              label: 'Name',
+              width: '50%',
+              sortable: true,
+              render: (row) => (
+                <a
+                  href={`https://${row.name}`}
+                  target="_blank"
+                  referrerPolicy="no-referrer"
+                >
+                  <IconText iconName="square-up-right">
+                    <Text tw="not-italic! font-bold!">{row.name}</Text>
+                  </IconText>
+                </a>
+              ),
+            },
+            {
+              label: 'Target',
+              sortable: true,
+              render: (row) => EntityDomainTypeName[row.target],
+            },
+            {
+              label: 'Ref',
+              width: '50%',
+              sortable: true,
+              render: (row) => {
+                if (row.target === EntityDomainType.IPFS) {
+                  return row.ref
+                }
 
-                  return entityNames.get(row.ref) ?? row.ref
-                },
-                cellProps: () => ({
-                  css: tw`max-w-0 whitespace-nowrap overflow-hidden text-ellipsis px-3!`,
-                }),
+                return entityNames.get(row.ref) ?? row.ref
               },
-              {
-                label: '',
-                align: 'right',
-                render: (row) => (
-                  <ButtonLink
-                    kind="functional"
-                    variant="secondary"
-                    href={`/console/settings/domain/${row.id}`}
-                  >
-                    <Icon name="angle-right" size="lg" />
-                  </ButtonLink>
-                ),
-                cellProps: () => ({
-                  css: tw`pl-3!`,
-                }),
-              },
-            ]}
-          />
-        </div>
-      </NoisyContainer>
+              cellProps: () => ({
+                css: tw`max-w-0 whitespace-nowrap overflow-hidden text-ellipsis px-3!`,
+              }),
+            },
+            {
+              label: '',
+              align: 'right',
+              render: (row) => (
+                <ButtonLink
+                  kind="functional"
+                  variant="secondary"
+                  href={`/console/settings/domain/${row.id}`}
+                >
+                  <Icon name="angle-right" size="lg" />
+                </ButtonLink>
+              ),
+              cellProps: () => ({
+                css: tw`pl-3!`,
+              }),
+            },
+          ]}
+        />
+      </div>
       {cta && (
         <div tw="mt-10 text-center">
           <ButtonLink variant="primary" href="/console/settings/domain/new">
--- src/components/pages/console/volume/VolumesTabContent/cmp.tsx
+++ src/components/pages/console/volume/VolumesTabContent/cmp.tsx
@@ -4,7 +4,7 @@ import { VolumesTabContentProps } from './types'
 import ButtonLink from '@/components/common/ButtonLink'
 import { ellipseAddress, humanReadableSize } from '@/helpers/utils'
 import EntityTable from '@/components/common/EntityTable'
-import { Icon, NoisyContainer } from '@aleph-front/core'
+import { Icon } from '@aleph-front/core'
 import { NAVIGATION_URLS } from '@/helpers/constants'
 
 export const VolumesTabContent = ({
@@ -13,55 +13,53 @@ export const VolumesTabContent = ({
 }: VolumesTabContentProps) => {
   return (
     <>
-      <NoisyContainer>
-        <div tw="overflow-auto max-w-full">
-          <EntityTable
-            borderType="none"
-            rowNoise
-            rowKey={(row) => row.id}
-            data={data}
-            rowProps={(row) => ({
-              css: row.confirmed ? '' : tw`opacity-60`,
-            })}
-            columns={[
-              {
-                label: 'Name',
-                width: '100%',
-                sortable: true,
-                render: (row) => ellipseAddress(row.id || ''),
-              },
-              {
-                label: 'Size',
-                align: 'right',
-                sortable: true,
-                render: (row) => humanReadableSize(row.size || 0, 'MiB'),
-              },
-              {
-                label: 'Date',
-                align: 'right',
-                sortable: true,
-                render: (row) => row.date,
-              },
-              {
-                label: '',
-                align: 'right',
-                render: (row) => (
-                  <ButtonLink
-                    kind="functional"
-                    variant="secondary"
-                    href={`${NAVIGATION_URLS.console.storage.volumes.home}/${row.id}`}
-                  >
-                    <Icon name="angle-right" size="lg" />
-                  </ButtonLink>
-                ),
-                cellProps: () => ({
-                  css: tw`pl-3!`,
-                }),
-              },
-            ]}
-          />
-        </div>
-      </NoisyContainer>
+      <div tw="overflow-auto max-w-full">
+        <EntityTable
+          borderType="none"
+          rowNoise
+          rowKey={(row) => row.id}
+          data={data}
+          rowProps={(row) => ({
+            css: row.confirmed ? '' : tw`opacity-60`,
+          })}
+          columns={[
+            {
+              label: 'Name',
+              width: '100%',
+              sortable: true,
+              render: (row) => ellipseAddress(row.id || ''),
+            },
+            {
+              label: 'Size',
+              align: 'right',
+              sortable: true,
+              render: (row) => humanReadableSize(row.size || 0, 'MiB'),
+            },
+            {
+              label: 'Date',
+              align: 'right',
+              sortable: true,
+              render: (row) => row.date,
+            },
+            {
+              label: '',
+              align: 'right',
+              render: (row) => (
+                <ButtonLink
+                  kind="functional"
+                  variant="secondary"
+                  href={`${NAVIGATION_URLS.console.storage.volumes.home}/${row.id}`}
+                >
+                  <Icon name="angle-right" size="lg" />
+                </ButtonLink>
+              ),
+              cellProps: () => ({
+                css: tw`pl-3!`,
+              }),
+            },
+          ]}
+        />
+      </div>
       {cta && (
         <div tw="mt-10 text-center">
           <ButtonLink
