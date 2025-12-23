444195a8bddc7fbd8854093272124adb27e835f0
Gerard Molina <47140788+gmolki@users.noreply.github.com>
2025-12-23 17:46:37 +0100

    refactor: clickable table rows style (#190)
    * refactor/fix: clickable tables styles
    
    * fix: clickable rows for select tables

---
+++ src/components/common/Table/types.ts
@@ -0,0 +1,32 @@
+type ThemeColorKey =
+  | 'white'
+  | 'black'
+  | 'translucid'
+  | 'base0'
+  | 'base1'
+  | 'base2'
+  | 'main0'
+  | 'main1'
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'error'
+  | 'disabled'
+  | 'disabled2'
+  | 'background'
+  | 'contentBackground'
+  | 'foreground'
+  | 'text'
+  | 'light0'
+  | 'light1'
+  | 'purple0'
+  | 'purple1'
+  | 'purple2'
+  | 'purple3'
+  | 'purple4'
+
+export type StyledTableProps = {
+  clickableRows?: boolean
+  rowBackgroundColors?: [ThemeColorKey, ThemeColorKey]
+  hoverHighlight?: boolean
+}
