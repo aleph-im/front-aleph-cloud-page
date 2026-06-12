1ae80d224516bedd3e28d3f6b3102c8650ddd694
Ali EL BROUDI <ali.elb@gmail.com>
2026-06-12 21:20:44 +0200

    feat: split metrics source address and dedupe hosting provider chart
      Add a dedicated metricAddress constant for the aleph-network-metrics
      post, separate from scoringAddress which now points to a different
      account. Add optional duplicate_ip to BaseNodeScoreMeasurements and
      filter duplicate-IP nodes out of the top hosting provider counts.

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -15,6 +15,7 @@ export const mbPerAleph = 3
 export const communityWalletAddress =
   '0x5aBd3258C5492fD378EBC2e0017416E199e5Da56'
 export const scoringAddress = '0x4D52380D3191274a04846c89c069E6C3F2Ed94e4'
+export const metricAddress = '0x4D52380D3191274a04846c89c069E6C3F2Ed94e4'
 export const monitorAddress = '0xa1B3bb7d2332383D96b7796B908fB7f7F3c2Be10'
 export const senderAddress = '0x3a5CC6aBd06B601f4654035d125F9DD2FC992C25'
 export const erc20Address = '0x27702a26126e0B3702af63Ee09aC4d1A084EF628'
